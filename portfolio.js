0867993b3f0ec19fd972b1fce9c0ee6c90d5e5c2
Martin Galara <mgalara@gmail.com>
2022-12-12 17:53:57 -0300

    imagenes nuevas, con responsive

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,37 @@
-//asd
+
+
+let menu = document.getElementById('menu');
+let toggle_open = document.getElementById('toggle_open');
+let toggle_close = document.getElementById('toggle_close');
+
+toggle_open.addEventListener('click', toggleMenu);
+toggle_close.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  menu.classList.toggle('show-menu');
+
+  if(menu.classList.contains('show-menu')) {
+    toggle_open.style.display = 'none';
+    toggle_close.style.display = 'block';
+  }
+  else {
+    toggle_open.style.display = 'block';
+    toggle_close.style.display = 'none';
+  }
+}
+
+let home = document.getElementById('home_navbar');
+let about_me = document.getElementById('about_me_navbar');
+let portfolio = document.getElementById('portfolio_navbar');
+let contact = document.getElementById('contact_navbar');
+home.addEventListener('click', toggleMenuByNav);
+about_me.addEventListener('click', toggleMenuByNav);
+portfolio.addEventListener('click', toggleMenuByNav);
+contact.addEventListener('click', toggleMenuByNav);
+
+function toggleMenuByNav() {
+  console.log("hola")
+    menu.classList.toggle('show-menu');
+    toggle_open.style.display = 'block';
+    toggle_close.style.display = 'none';
+}
